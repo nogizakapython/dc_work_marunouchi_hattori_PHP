75528ee954f90a0c26736b1ac345f3c231c7c78d
Takao Hattori <nogizakapython@outlook.jp>
2023-02-24 10:56:56 +0900

    Modefy20230224Morning

---
+++ kensaku/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>近所の飲食店検索</title>
+  <link rel="stylesheet" href="common.css">
+</head>
+<body>
+  <h2 class="title1">近所の飲食店の登録、検索サイト</h2>
+  <div class="item1">
+    <ul class="list1">
+      <li><a href="entry.php">登録</a></li>
+      <li><a href="kensaku.php">検索</a></li>
+      <li><a href="reject.php">削除</a></li>
+    </ul>
+  </div>
+</body>
+</html>
